f6a0011bd41c96380c2f2c5568568dcbe1031141
DarrenVs <DarrenVanstaen@Gmail.com>
2015-11-02 16:31:16 +0100

    Physics scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public GameObject world;
+
+    Rigidbody2D rig;
+    Vector3 horizontal;
+    Vector3 vertical;
+
+    // Use this for initialization
+    void Start () {
+        rig = gameObject.GetComponent<Rigidbody2D>();
+
+        Time.timeScale = 10;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        horizontal = transform.right * Input.GetAxis("Horizontal");
+        vertical = transform.up * Input.GetAxis("Vertical");
+
+        rig.AddForce ((horizontal + vertical) * Time.deltaTime * 1000);
+
+	}
+}
