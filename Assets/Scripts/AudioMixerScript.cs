8708e83135b56c322b16616a7cbd6fd1f552d365
Ezowolf <wiggler@live.nl>
2015-11-02 15:01:48 +0100

    Here make a mixtape for free easy no music required

---
+++ Assets/Scripts/AudioMixerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioMixerScript : MonoBehaviour {
+
+	public AudioMixer MasterMixer;
+
+	public void SetALevel(float ALevel)
+	{
+		MasterMixer.SetFloat("GroupAVol", ALevel);
+	}
+
+	public void SetBLevel(float BLevel)
+	{
+		MasterMixer.SetFloat("GroupBVol", BLevel);
+	}
+
+	public void SetCLevel(float CLevel)
+	{
+		MasterMixer.SetFloat("GroupCVol", CLevel);
+	}
+
+	public void SetDLevel(float DLevel)
+	{
+		MasterMixer.SetFloat("GroupDVol", DLevel);
+	}
+
+	public void SetMasterLevel(float MasterLevel)
+	{
+		MasterMixer.SetFloat("MasterVol", MasterLevel);
+	}
+}
