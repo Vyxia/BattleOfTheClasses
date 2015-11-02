f9f09491775171a1f2cb20b968855cfef279ece9
18839 <18839@ma-web.nl>
2015-11-02 16:06:45 +0100

    NavMesh
    Ferry en Daniël GD2A

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 distance;
+    [SerializeField]
+    private int rotation;
+    [SerializeField]
+    private Transform player;
+
+    void Awake()
+    {
+        this.transform.eulerAngles = Vector3.right * rotation;
+    }
+
+    void Update()
+    {
+        // Set the position to the player's position with the offset.
+        if (player != null) this.transform.position = player.position + distance;
+    }
+}
---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour
+{
+    [SerializeField]
+    private Transform goal;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = 3f;
+    }
+
+    void Update()
+    {
+        agent.destination = goal.position;
+    }
+}
