ed999c3ae19271799a14a25b2949205d4dcee80b
nathan-san <18964@ma-web.nl>
2015-11-02 13:18:18 +0100

    LoD toegevoegd.

---
+++ Assets/LODassets/LODModels/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	[SerializeField]
+	private float rotateSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+	}
+}
---
+++ Assets/LODassets/LODModels/Scrolldistance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Scrolldistance : MonoBehaviour {
+    //public Slider slider;
+    public Scrollbar slider;
+    float value = 0f;
+    public float valueMultiplier;
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        value = slider.value;
+        Vector3 temp = new Vector3(0f, 0f, value * valueMultiplier);
+        transform.position = temp;
+	}
+}
